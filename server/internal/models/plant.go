9be282e3ac37e6f7be42a650b4eeeb97452b1682
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2026-02-07 15:33:48 +0800

    models: plant & plantSettings

---
+++ server/internal/models/plant.go
@@ -0,0 +1,19 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Plant struct {
+	ID          uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();primaryKey" json:"id"`
+	Name        string    `gorm:"type:varchar(100);not null" json:"name"`
+	Location    string    `gorm:"type:varchar(100);not null" json:"location"`
+	Description *string   `gorm:"type:varchar(100)" json:"description,omitempty"`
+
+	CreatedAt time.Time `gorm:"autoCreateTime" json:"createdAt"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updatedAt"`
+
+	Settings PlantSettings `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"settings"`
+}
