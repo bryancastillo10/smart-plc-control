9a456003bb012ae853007006eda0fc3c7d263c47
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-02-09 09:04:20 +0800

    📂 structure: internal folder by feature, initialize simulator

--- server/infrastructure/routes.go
+++ server/infrastructure/routes.go
@@ -1,15 +1,15 @@
 package infrastructure
 
 import (
-	"plc-dashboard/internal/handler"
+	"plc-dashboard/internal/simulator"
 
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterRoutes(r *gin.Engine) {
-	r.GET("/", func (c *gin.Context) {
-		c.JSON(200, gin.H {"message": "🌊 PLC-Simulator Server"})
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "🌊 PLC-Simulator Server"})
 	})
 
-	r.GET("ws/plc", handler.SensorStream)
+	r.GET("ws/plc", simulator.SensorStream)
 }
--- server/internal/simulator/service.go
+++ server/internal/simulator/service.go
@@ -1,4 +1,4 @@
-package service
+package simulator
 
 import (
 	"math/rand"
@@ -8,14 +8,14 @@ import (
 	"plc-dashboard/internal/models"
 )
 
-func randomRange(min,max float64) float64 {
+func randomRange(min, max float64) float64 {
 	return min + rand.Float64()*(max-min)
 }
 
 func GenerateSensorData() models.PlcSensors {
 	return models.PlcSensors{
-		Timestamp: time.Now(),
-		FlowRate: randomRange(0.8,1.5),
+		Timestamp:       time.Now(),
+		FlowRate:        randomRange(0.8, 1.5),
 		PH:              randomRange(6.5, 8.2),
 		Turbidity:       randomRange(1.0, 10.0),
 		DissolvedOxygen: randomRange(4.0, 9.0),
