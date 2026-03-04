c49ac2ebe9c67534ba24428a07897791fc8a9407
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2026-03-05 04:35:16 +0800

    🌐 dto: added response structs

--- server/internal/valve/dto.go
+++ server/internal/valve/dto.go
@@ -4,3 +4,13 @@ type UpdateValveRequest struct {
 	Position *float64 `json:"position,omitempty" binding:"omitempty,min=0,max=100"`
 	IsAuto   *bool    `json:"isAuto,omitempty"`
 }
+
+type ValveResponse struct {
+	ID          string   `json:"id"`
+	PlantID     string   `json:"plantId"`
+	Name        string   `json:"name"`
+	Location    string   `json:"location"`
+	Description *string  `json:"description,omitempty"`
+	Position    float64  `json:"position"`
+	IsAuto      bool     `json:"isAuto"`
+}
