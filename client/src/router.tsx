9bd6f4ec5a85a20735cc8508dc58f8dac7b8e340
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-03-11 10:51:10 +0800

    ✨feat: react-query client provider usage

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,17 +1,31 @@
 import { createRouter } from '@tanstack/react-router'
+import { setupRouterSsrQueryIntegration } from "@tanstack/react-router-ssr-query";
+import * as TanstackQuery from "@/lib/tanstack-query/provider";
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
 
 // Create a new router instance
 export const getRouter = () => {
-  const router = createRouter({
-    routeTree,
-    context: {},
+	const rqContext = TanstackQuery.getContext();
 
-    scrollRestoration: true,
-    defaultPreloadStaleTime: 0,
-  })
+	const router = createRouter({
+		routeTree,
+		context: { ...rqContext },
+		defaultPreload: "intent",
+		Wrap: (props: { children: React.ReactNode }) => {
+			return (
+				<TanstackQuery.Provider {...rqContext}>
+					{props.children}
+				</TanstackQuery.Provider>
+			);
+		},
+	});
 
-  return router
-}
+	setupRouterSsrQueryIntegration({
+		router,
+		queryClient: rqContext.queryClient,
+	});
+
+	return router;
+};
