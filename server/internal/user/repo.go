be01e4246debc4ea3e389c21c99653d6bcfe4939
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-03-12 13:14:14 +0800

    🌐api: FindUserByID repo layer

--- server/internal/user/repo.go
+++ server/internal/user/repo.go
@@ -1,6 +1,10 @@
 package user
 
 import (
+	"errors"
+	"plc-dashboard/models"
+
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
@@ -12,3 +16,15 @@ func NewRepository(db *gorm.DB) *Repository {
 	return &Repository{db: db}
 }
 
+func (r *Repository) FindUserByID(userID uuid.UUID) (*models.User, error) {
+	var user models.User
+
+	if err := r.db.First(&user, "id = ?", userID).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return &user, nil
+}
