398bac714dbe051bfa960db689470d6da6bfe910
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-03-12 13:19:53 +0800

    🌐api: GetUser service layer

--- server/internal/user/service.go
+++ server/internal/user/service.go
@@ -1,9 +1,35 @@
 package user
 
+import (
+	appErr "plc-dashboard/pkg/errors"
+	"plc-dashboard/pkg/utils"
+)
+
 type Service struct {
 	repo *Repository
 }
 
 func NewService(repo *Repository) *Service {
 	return &Service{repo: repo}
+}
+
+
+
+func (s *Service) GetUser(userID string) (*UserData, error) {
+	uid, err := utils.ParseId(userID)
+	if err != nil {
+		return nil, appErr.NewBadRequest("Invalid user ID", err)
+	}
+
+	user, err := s.repo.FindUserByID(uid)
+	if err != nil {
+		return nil, appErr.NewInternal("Failed to retrieve user", err)
+	}
+	if user == nil {
+		return nil, appErr.NewNotFound("User not found", nil)
+	}
+
+	response := GetUserResponse(user)
+
+	return &response, nil
 }
