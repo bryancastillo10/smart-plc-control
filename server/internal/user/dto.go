0697165411d06d04732f6cb8597507b5fea577ba
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-03-12 13:17:50 +0800

    ✨feat: GetUserResponse

--- server/internal/user/dto.go
+++ server/internal/user/dto.go
@@ -1,6 +1,9 @@
 package user
 
-import "time"
+import (
+	"plc-dashboard/models"
+	"time"
+)
 
 type UserData struct {
 	ID        string    `json:"id"`
@@ -19,4 +22,17 @@ type UpdateUserRequest struct {
 
 type DeleteUserRequest struct {
 	UserName string `json:"username"`
+}
+
+
+func GetUserResponse(user *models.User) UserData {
+	return UserData{
+		ID:        user.ID.String(),
+		UserName:  user.UserName,
+		Email:     user.Email,
+		Role:      string(user.Role),
+		Language:  string(user.Language),
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+	}
 }
