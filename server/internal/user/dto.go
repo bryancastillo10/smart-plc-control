7508ce481a61e9957089d0e2143672a8e0aac523
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-03-12 13:13:08 +0800

    🧹chore: added timestamp on UserData

--- server/internal/user/dto.go
+++ server/internal/user/dto.go
@@ -1,15 +1,20 @@
 package user
 
+import "time"
+
 type UserData struct {
-	UserName	string `json:"username"`
-	Email	string `json:"email"`
-	Role	string `json:"role"`
-	Language	string `json:"lang"`
+	ID        string    `json:"id"`
+	UserName  string    `json:"username"`
+	Email     string    `json:"email"`
+	Role      string    `json:"role"`
+	Language  string    `json:"language"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
 }
 
 type UpdateUserRequest struct {
-	UserName	string `json:"username"`
-	Language	string `json:"lang"`
+	UserName string `json:"username"`
+	Language string `json:"lang"`
 }
 
 type DeleteUserRequest struct {
