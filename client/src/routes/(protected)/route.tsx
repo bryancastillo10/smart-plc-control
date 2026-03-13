71961eb68296058c5906a115d7b61392fa325946
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-03-13 13:44:00 +0800

    ✨feat: react-query client context at root usage

--- client/src/routes/(protected)/route.tsx
+++ client/src/routes/(protected)/route.tsx
@@ -1,18 +1,31 @@
-import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
 
 import Sidebar from "@/components/layout/sidebar";
+import { getCurrentUserQueryOptions } from "@/hooks/use-get-user";
 
 export const Route = createFileRoute("/(protected)")({
-  component: RouteComponent,
+	beforeLoad: async ({ context, location }) => {
+		try {
+			await context.queryClient.ensureQueryData(getCurrentUserQueryOptions());
+		} catch {
+			throw redirect({
+				to: "/sign-in",
+				search: {
+					redirect: location.href,
+				},
+			});
+		}
+	},
+	component: RouteComponent,
 });
 
 function RouteComponent() {
-  return (
-    <div className="flex min-h-screen w-full flex-col md:flex-row">
-      <Sidebar />
-      <main className="flex-1 overflow-y-auto p-4 md:p-6">
-        <Outlet />
-      </main>
-    </div>
-  );
+	return (
+		<div className="flex min-h-screen w-full flex-col md:flex-row">
+			<Sidebar />
+			<main className="flex-1 overflow-y-auto p-4 md:p-6">
+				<Outlet />
+			</main>
+		</div>
+	);
 }
