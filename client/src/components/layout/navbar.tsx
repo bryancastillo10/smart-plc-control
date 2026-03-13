ff7ce4ff2334d89d056aa91ccc921d5b379e36a4
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-03-13 13:48:31 +0800

    ✨feat: navbar action button can switch between sign in/sign out

--- client/src/components/layout/navbar.tsx
+++ client/src/components/layout/navbar.tsx
@@ -1,21 +1,27 @@
 import { Link } from "@tanstack/react-router";
 import { Factory } from "lucide-react";
-
-import { Button } from "@/components/ui/button";
-import SelectLanguage from "@/components/layout/select-language";
 import { useTranslation } from "react-i18next";
+import SelectLanguage from "@/components/layout/select-language";
+import { Button } from "@/components/ui/button";
+
+import useGetUser from "@/hooks/use-get-user";
+import useSignOut from "@/hooks/use-sign-out";
 
 const Navbar = () => {
+	const { isAuthenticated, isLoading } = useGetUser();
+	const { handleSignout, loading: isSigningOut } = useSignOut();
 	const { t } = useTranslation();
+
+	const logoLink = isAuthenticated ? "/dashboard" : "/";
 
 	return (
 		<header className="sticky top-0 z-50 w-full border-b border-border/50 bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/70">
 			<div className="mx-auto flex h-16 w-full max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
 				<div className="flex items-center gap-3">
-					<Link to="/">
-					<div className="rounded-md bg-primary/10 p-2 text-primary">
-						<Factory aria-hidden="true" className="size-5" />
-					</div>
+					<Link to={logoLink}>
+						<div className="rounded-md bg-primary/10 p-2 text-primary">
+							<Factory aria-hidden="true" className="size-5" />
+						</div>
 					</Link>
 					<div className="flex flex-col">
 						<span className="text-sm text-muted-foreground">
@@ -28,11 +34,22 @@ const Navbar = () => {
 				</div>
 
 				<div className="flex items-center gap-2">
-					<Button asChild size="sm" variant="default">
-						<Link to="/sign-in">{t("signIn")}</Link>
-					</Button>	
+					{isLoading ? null : isAuthenticated ? (
+						<Button
+							disabled={isSigningOut}
+							onClick={handleSignout}
+							size="sm"
+							variant="secondary"
+						>
+							{isSigningOut ? t("signingOut") : t("signOut")}
+						</Button>
+					) : (
+						<Button asChild size="sm" variant="default">
+							<Link to="/sign-in">{t("signIn")}</Link>
+						</Button>
+					)}
 
-					<SelectLanguage/>
+					<SelectLanguage />
 				</div>
 			</div>
 		</header>
