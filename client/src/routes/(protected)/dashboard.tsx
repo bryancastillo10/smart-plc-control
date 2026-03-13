cf36a77a5fe1e406a5ccde89bf4bd4b4fdf7a85c
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-03-13 13:49:02 +0800

    🎉init: dashboard page

--- client/src/routes/(protected)/dashboard.tsx
+++ client/src/routes/(protected)/dashboard.tsx
@@ -1,11 +1,18 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import useGetUser from "@/hooks/use-get-user";
 
-export const Route = createFileRoute('/(protected)/dashboard')({
-  component: RouteComponent,
-})
+export const Route = createFileRoute("/(protected)/dashboard")({
+	component: RouteComponent,
+});
 
 function RouteComponent() {
-  return <div>
-      <h1>Dashboard Page</h1>
-    </div>
+	const { authUser } = useGetUser();
+
+
+	return (
+		<div>
+			<h1>Dashboard Page</h1>
+			<p>Welcome {authUser?.email} </p>
+		</div>
+	);
 }
