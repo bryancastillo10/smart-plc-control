4bb1f4357271631ecac58846fd3d415a6bde47e7
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-03-23 11:12:03 +0800

    ✨feat: added home page beforeLoad config

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,24 +1,34 @@
-import { createFileRoute } from "@tanstack/react-router";
-
+import { createFileRoute, redirect } from "@tanstack/react-router";
+import { getCurrentUserQueryOptions } from "@/hooks/use-get-user";
 
 import HeroTitle from "@/components/home/hero-title";
 import HeroPreview from "@/components/home/hero-preview";
 import Features from "@/components/home/features";
 
-export const Route = createFileRoute("/")({ component: App });
+export const Route = createFileRoute("/")({
+	beforeLoad: async ({ context }) => {
+		const authUser = await context.queryClient
+			.ensureQueryData(getCurrentUserQueryOptions())
+			.catch(() => null);
+
+		if (authUser) {
+			throw redirect({ to: "/dashboard" });
+		}
+	},
+	component: App,
+});
 
 function App() {
+	return (
+		<main className="relative min-h-[calc(100vh-4rem)] overflow-hidden bg-linear-to-br from-zinc-950 via-slate-900 to-cyan-950 text-slate-100">
+			<div className="pointer-events-none absolute -left-36 top-10 h-80 w-80 rounded-full bg-cyan-400/20 blur-3xl" />
+			<div className="pointer-events-none absolute -right-32 bottom-0 h-72 w-72 rounded-full bg-emerald-400/15 blur-3xl" />
 
-  return (
-    <main className="relative min-h-[calc(100vh-4rem)] overflow-hidden bg-linear-to-br from-zinc-950 via-slate-900 to-cyan-950 text-slate-100">
-      <div className="pointer-events-none absolute -left-36 top-10 h-80 w-80 rounded-full bg-cyan-400/20 blur-3xl" />
-      <div className="pointer-events-none absolute -right-32 bottom-0 h-72 w-72 rounded-full bg-emerald-400/15 blur-3xl" />
-
-      <section className="mx-auto grid w-full max-w-7xl gap-10 px-6 py-16 md:grid-cols-2 md:items-center md:py-24">
-        <HeroTitle/>
-        <HeroPreview/>
-      </section>
-        <Features/>
-    </main>
-  );    
+			<section className="mx-auto grid w-full max-w-7xl gap-10 px-6 py-16 md:grid-cols-2 md:items-center md:py-24">
+				<HeroTitle />
+				<HeroPreview />
+			</section>
+			<Features />
+		</main>
+	);
 }
