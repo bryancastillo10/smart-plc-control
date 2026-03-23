c9b26534e68bd20e7e1bc7b2b98a53d4a7e4b8b0
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-03-23 13:15:02 +0800

    🎉init: ws endpoint vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -19,6 +19,12 @@ const config = defineConfig({
 				changeOrigin: true,
 				secure: false,
 			},
+			"/ws": {
+				target: "http://localhost:8080",
+				changeOrigin: true,
+				secure: false,
+				ws: true,
+			},
 		},
 	},
 	plugins: [
