2af2eef4161e2d55744bebb2ef1ec013f68ddd27
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-06-22 15:48:53 +0800

    ✨feat: error handling middleware

--- server/bootstrap/run_gin.go
+++ server/bootstrap/run_gin.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+
+	"smart-plc-control-server/internal/middleware"
 )
 
 func RunGin(config cors.Config) {
@@ -25,7 +27,7 @@ func RunGin(config cors.Config) {
 		}
 	}
 
-	// r.Use(middleware.ErrorHandler())
+	r.Use(middleware.ErrorHandler())
 
 	RegisterRoutes(r)
 
