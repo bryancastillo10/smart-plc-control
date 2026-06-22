df7be61a07c5f3f6dc574e5ed94f2a831449cd6b
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-06-22 14:56:49 +0800

    🧹chore: users model update

--- server/internal/models/users.go
+++ server/internal/models/users.go
@@ -7,12 +7,12 @@ import (
 )
 
 type Users struct {
-	ID       uuid.UUID `gorm:"primaryKey;type:uuid" json:"id"`
-	UserName string    `gorm:"type:varchar(100);not null" json:"userName"`
-	Email    string    `gorm:"type:varchar(100);uniqueIndex;not null" json:"email"`
-	Password string    `gorm:"not null" json:"password"`
-	Language Language  `gorm:"type:varchar(20);not null;default:EN" json:"language"`
-	Role     Role      `gorm:"type:varchar(20)" json:"role"`
+	ID           uuid.UUID `gorm:"primaryKey;type:uuid" json:"id"`
+	UserName     string    `gorm:"column:username;type:varchar(100);not null" json:"username"`
+	Email        string    `gorm:"type:varchar(255);uniqueIndex;not null" json:"email"`
+	PasswordHash string    `gorm:"column:password_hash;type:text;not null" json:"-"`
+	Language     Language  `gorm:"type:varchar(10);not null;default:EN" json:"language"`
+	Role         Role      `gorm:"type:varchar(20);not null;default:VIEWER" json:"role"`
 
 	CreatedAt time.Time `gorm:"autoCreateTime" json:"createdAt"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updatedAt"`
