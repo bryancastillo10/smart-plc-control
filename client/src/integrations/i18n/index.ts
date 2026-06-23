be34e2c951d2a1e1e869a82e3969d97efc8378e0
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-06-23 10:57:00 +0800

    ✨feat: i18next config

---
+++ client/src/integrations/i18n/index.ts
@@ -0,0 +1,29 @@
+import i18n from "i18next"
+import { initReactI18next } from "react-i18next"
+
+import { en } from "./locales/en.ts"
+import { zhTW } from "./locales/zh-TW.ts"
+
+export const supportedLanguages = ["EN", "ZH-TW"] as const
+export type Language = (typeof supportedLanguages)[number]
+
+export const i18nLanguageByApiLanguage = {
+	EN: "en",
+	"ZH-TW": "zh-TW",
+} satisfies Record<Language, string>
+
+i18n.use(initReactI18next).init({
+	resources: {
+		en,
+		"zh-TW": zhTW,
+	},
+	lng: "en",
+	fallbackLng: "en",
+	defaultNS: "login",
+	supportedLngs: ["en", "zh-TW"],
+	interpolation: {
+		escapeValue: false,
+	},
+})
+
+export default i18n
