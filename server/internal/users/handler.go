9e832ec1a7d4635e6c19cb049ba76919813f33f2
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-06-26 11:03:17 +0800

    🌐api: user handler layer

--- server/internal/users/handler.go
+++ server/internal/users/handler.go
@@ -1,6 +1,11 @@
 package users
 
-import "gorm.io/gorm"
+import (
+	"smart-plc-control-server/pkg/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
 
 type Handler struct {
 	service *Service
@@ -10,4 +15,32 @@ func NewHandler(db *gorm.DB) *Handler {
 	repo := NewRepository(db)
 	service := NewService(repo)
 	return &Handler{service: service}
+}
+
+func (h *Handler) GetAllUsers(c *gin.Context) {
+	users, err := h.service.GetAllUsers()
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	c.JSON(200, users)
+}
+
+func (h *Handler) UpdateUser(c *gin.Context) {
+	userID := c.Param("userId")
+
+	req, err := http.BindJSON[UpdateUserRequest](c)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	user, err := h.service.UpdateUser(userID, *req)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	c.JSON(200, user)
 }
