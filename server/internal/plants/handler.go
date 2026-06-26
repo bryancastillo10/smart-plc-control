6c2607502877ab307b4e0aec32f4fb7c5fa45338
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-06-26 10:08:09 +0800

    ✨init: internal/plants

---
+++ server/internal/plants/handler.go
@@ -0,0 +1,13 @@
+package plants
+
+import "gorm.io/gorm"
+
+type Handler struct {
+	service *Service
+}
+
+func NewHandler(db *gorm.DB) *Handler {
+	repo := NewRepository(db)
+	service := NewService(repo)
+	return &Handler{service: service}
+}
