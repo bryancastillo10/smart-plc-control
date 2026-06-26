f476d9d74281666f0a97dc99b1af118a1c2013fc
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-06-26 11:02:12 +0800

    🌐api: users repo layer findAllUser, findUserById & UpdateUser

--- server/internal/users/repo.go
+++ server/internal/users/repo.go
@@ -1,6 +1,10 @@
 package users
 
-import "gorm.io/gorm"
+import (
+	"smart-plc-control-server/internal/models"
+
+	"gorm.io/gorm"
+)
 
 type Repository struct {
 	db *gorm.DB
@@ -8,4 +12,33 @@ type Repository struct {
 
 func NewRepository(db *gorm.DB) *Repository {
 	return &Repository{db: db}
+}
+
+func (r *Repository) FindAllUsers() ([]models.Users, error) {
+	var users []models.Users
+	if err := r.db.Order("created_at DESC").Find(&users).Error; err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
+
+func (r *Repository) FindUserByID(userID string) (*models.Users, error) {
+	var user models.Users
+	if err := r.db.Where("id = ?", userID).First(&user).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (r *Repository) UpdateUser(user *models.Users) (*models.Users, error) {
+	if err := r.db.Save(user).Error; err != nil {
+		return nil, err
+	}
+
+	return user, nil
 }
