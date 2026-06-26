340a690b886155f4d10df16ebfb68eb37c7c402f
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-06-26 10:13:37 +0800

    ✨init: internal/audit_logs

---
+++ server/internal/audit_logs/handler.go
@@ -0,0 +1,13 @@
+package audit_logs
+
+import "gorm.io/gorm"
+
+type Handler struct {
+	service *Service
+}
+
+func NewHandler(db *gorm.DB) *Handler {
+	repo := NewRepository(db)
+	service := NewService(repo)
+	return &Handler{service: service}
+}
