8a3f9fa8e4ed9219972b74d52fd56e7611320be6
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-06-30 14:57:01 +0800

    🌐api: create plant repo layer

--- server/internal/plants/repo.go
+++ server/internal/plants/repo.go
@@ -1,6 +1,10 @@
 package plants
 
-import "gorm.io/gorm"
+import (
+	"smart-plc-control-server/internal/models"
+
+	"gorm.io/gorm"
+)
 
 type Repository struct {
 	db *gorm.DB
@@ -8,4 +12,12 @@ type Repository struct {
 
 func NewRepository(db *gorm.DB) *Repository {
 	return &Repository{db: db}
+}
+
+func (r *Repository) CreatePlant(plant *models.Plants) (*models.Plants, error) {
+	if err := r.db.Create(plant).Error; err != nil {
+		return nil, err
+	}
+
+	return plant, nil
 }
