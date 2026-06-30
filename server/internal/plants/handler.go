05aedbf7cddaec9d6c6231cf0b57af8586123afa
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-06-30 15:03:35 +0800

    🌐api: create plant handler layer

--- server/internal/plants/handler.go
+++ server/internal/plants/handler.go
@@ -1,6 +1,11 @@
 package plants
 
-import "gorm.io/gorm"
+import (
+	"smart-plc-control-server/pkg/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
 
 type Handler struct {
 	service *Service
@@ -10,4 +15,20 @@ func NewHandler(db *gorm.DB) *Handler {
 	repo := NewRepository(db)
 	service := NewService(repo)
 	return &Handler{service: service}
+}
+
+func (h *Handler) CreatePlant(c *gin.Context) {
+	req, err := http.BindJSON[CreatePlantRequest](c)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	plant, err := h.service.CreatePlant(*req)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	c.JSON(201, plant)
 }
