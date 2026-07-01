e4b70196143a2ab148d56259f60426172345d4f6
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-07-01 09:53:36 +0800

    🌐api: createProcessUnit handler layer

--- server/internal/process_units/handler.go
+++ server/internal/process_units/handler.go
@@ -1,6 +1,11 @@
 package process_units
 
-import "gorm.io/gorm"
+import (
+	"smart-plc-control-server/pkg/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
 
 type Handler struct {
 	service *Service
@@ -10,4 +15,22 @@ func NewHandler(db *gorm.DB) *Handler {
 	repo := NewRepository(db)
 	service := NewService(repo)
 	return &Handler{service: service}
+}
+
+func (h *Handler) CreateProcessUnit(c *gin.Context) {
+	plantID := c.Param("plantId")
+
+	req, err := http.BindJSON[CreateProcessUnitRequest](c)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	processUnit, err := h.service.CreateProcessUnit(plantID, *req)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	c.JSON(201, processUnit)
 }
