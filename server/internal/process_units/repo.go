1a5fe8883275b33dda25241cfba81b523abe3da4
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-07-01 10:51:57 +0800

    🌐api: update process units repo layer

--- server/internal/process_units/repo.go
+++ server/internal/process_units/repo.go
@@ -43,3 +43,23 @@ func (r *Repository) FindProcessUnitsByPlantID(plantID uuid.UUID) ([]models.Proc
 
 	return processUnits, nil
 }
+
+func (r *Repository) FindProcessUnitByID(processUnitID uuid.UUID) (*models.ProcessUnits, error) {
+	var processUnit models.ProcessUnits
+	if err := r.db.Where("id = ?", processUnitID).First(&processUnit).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return &processUnit, nil
+}
+
+func (r *Repository) UpdateProcessUnit(processUnit *models.ProcessUnits) (*models.ProcessUnits, error) {
+	if err := r.db.Save(processUnit).Error; err != nil {
+		return nil, err
+	}
+
+	return processUnit, nil
+}
