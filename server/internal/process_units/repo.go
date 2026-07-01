63f99aa9e5495db2a1bc868044614bf2e89e1250
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-07-01 09:47:23 +0800

    🌐api: findplantbyID & createProcessUnit repo layer

--- server/internal/process_units/repo.go
+++ server/internal/process_units/repo.go
@@ -1,6 +1,11 @@
 package process_units
 
-import "gorm.io/gorm"
+import (
+	"smart-plc-control-server/internal/models"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 type Repository struct {
 	db *gorm.DB
@@ -8,4 +13,24 @@ type Repository struct {
 
 func NewRepository(db *gorm.DB) *Repository {
 	return &Repository{db: db}
+}
+
+func (r *Repository) FindPlantByID(plantID uuid.UUID) (*models.Plants, error) {
+	var plant models.Plants
+	if err := r.db.Where("id = ?", plantID).First(&plant).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return &plant, nil
+}
+
+func (r *Repository) CreateProcessUnit(processUnit *models.ProcessUnits) (*models.ProcessUnits, error) {
+	if err := r.db.Create(processUnit).Error; err != nil {
+		return nil, err
+	}
+
+	return processUnit, nil
 }
