e9867cd8e28c5e3317bda16d9087f6170c710659
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-07-02 09:34:24 +0800

    📝types: user

---
+++ client/src/types/user.ts
@@ -0,0 +1,11 @@
+﻿import type { Language, UserRole } from "@/types/enum";
+
+export interface UserProfile {
+	id: string;
+	userName: string;
+	email: string;
+	role: UserRole;
+	language: Language;
+	createdAt: string;
+	updatedAt: string;
+}
