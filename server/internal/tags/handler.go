0104beafc222b2963b527c63f558b2a37f1d309c
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2026-07-04 21:22:21 +0800

    🌐api: create tags handler layer

--- server/internal/tags/handler.go
+++ server/internal/tags/handler.go
@@ -1,6 +1,11 @@
 package tags
 
-import "gorm.io/gorm"
+import (
+	"smart-plc-control-server/pkg/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
 
 type Handler struct {
 	service *Service
@@ -10,4 +15,22 @@ func NewHandler(db *gorm.DB) *Handler {
 	repo := NewRepository(db)
 	service := NewService(repo)
 	return &Handler{service: service}
+}
+
+func (h *Handler) CreateTag(c *gin.Context) {
+	deviceID := c.Param("deviceId")
+
+	req, err := http.BindJSON[CreateTagRequest](c)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	tag, err := h.service.CreateTag(deviceID, *req)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	c.JSON(201, tag)
 }
