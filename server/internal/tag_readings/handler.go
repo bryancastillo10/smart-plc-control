c311e863f91918a5971a779a3b5b83f0b4635885
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-07-06 11:40:26 +0800

    🌐 api: get latest readings handler layer

--- server/internal/tag_readings/handler.go
+++ server/internal/tag_readings/handler.go
@@ -1,6 +1,11 @@
 package tag_readings
 
-import "gorm.io/gorm"
+import (
+	"smart-plc-control-server/pkg/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
 
 type Handler struct {
 	service *Service
@@ -10,4 +15,20 @@ func NewHandler(db *gorm.DB) *Handler {
 	repo := NewRepository(db)
 	service := NewService(repo)
 	return &Handler{service: service}
+}
+
+func (h *Handler) GetLatestReadings(c *gin.Context) {
+	query, err := http.BindQuery[LatestReadingsQuery](c)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	readings, err := h.service.GetLatestReadings(*query)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	c.JSON(200, readings)
 }
