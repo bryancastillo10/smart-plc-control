5cfc915fc1521f9890d2659f0c6ee7be94cd7a33
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-07-07 10:59:06 +0800

    🌐api: get alerts handler layer

--- server/internal/alerts/handler.go
+++ server/internal/alerts/handler.go
@@ -1,6 +1,11 @@
 package alerts
 
-import "gorm.io/gorm"
+import (
+	"smart-plc-control-server/pkg/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
 
 type Handler struct {
 	service *Service
@@ -10,4 +15,20 @@ func NewHandler(db *gorm.DB) *Handler {
 	repo := NewRepository(db)
 	service := NewService(repo)
 	return &Handler{service: service}
+}
+
+func (h *Handler) GetAlerts(c *gin.Context) {
+	query, err := http.BindQuery[ListAlertsQuery](c)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	alerts, err := h.service.GetAlerts(*query)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	c.JSON(200, alerts)
 }
