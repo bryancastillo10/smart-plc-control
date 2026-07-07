8f3e4e9b59e88404dbd78aa45039039edb212363
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-07-07 10:21:59 +0800

    🔗route: connect delete alert rules handler at routing

--- server/bootstrap/routes.go
+++ server/bootstrap/routes.go
@@ -163,7 +163,7 @@ func registerAlertRules(r *gin.RouterGroup, DB *gorm.DB) {
 		alertRuleGrp.GET("", alertRule.GetAlertRules)
 		alertRuleGrp.GET("/:ruleId", alertRule.GetAlertRuleByID)
 		alertRuleGrp.PUT("/:ruleId", middleware.RequireRoles(models.Admin), alertRule.UpdateAlertRule)
-		alertRuleGrp.DELETE("/:ruleId")
+		alertRuleGrp.DELETE("/:ruleId", middleware.RequireRoles(models.Admin, models.Operator), alertRule.DeleteAlertRule)
 	}
 }
 
