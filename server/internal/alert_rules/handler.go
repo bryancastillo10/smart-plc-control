c81b5b26e2165b1487e7ca6ac43817651b5f1aa2
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-07-07 09:02:18 +0800

    🌐api: create alert rule handler layer

--- server/internal/alert_rules/handler.go
+++ server/internal/alert_rules/handler.go
@@ -1,6 +1,11 @@
 package alert_rules
 
-import "gorm.io/gorm"
+import (
+	"smart-plc-control-server/pkg/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
 
 type Handler struct {
 	service *Service
@@ -10,4 +15,20 @@ func NewHandler(db *gorm.DB) *Handler {
 	repo := NewRepository(db)
 	service := NewService(repo)
 	return &Handler{service: service}
+}
+
+func (h *Handler) CreateAlertRule(c *gin.Context) {
+	req, err := http.BindJSON[CreateAlertRuleRequest](c)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	alertRule, err := h.service.CreateAlertRule(*req)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	c.JSON(201, alertRule)
 }
