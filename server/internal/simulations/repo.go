e4d3aaacdf979855ffb76669313d500778acc248
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-07-09 16:42:03 +0800

    🌐api: create simulations repo layer

--- server/internal/simulations/repo.go
+++ server/internal/simulations/repo.go
@@ -1,6 +1,11 @@
 package simulations
 
-import "gorm.io/gorm"
+import (
+	"smart-plc-control-server/internal/models"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 type Repository struct {
 	db *gorm.DB
@@ -8,4 +13,24 @@ type Repository struct {
 
 func NewRepository(db *gorm.DB) *Repository {
 	return &Repository{db: db}
+}
+
+func (r *Repository) FindPlantByID(plantID uuid.UUID) (*models.Plants, error) {
+	var plant models.Plants
+	if err := r.db.Where("id = ?", plantID).First(&plant).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return &plant, nil
+}
+
+func (r *Repository) CreateSimulation(simulation *models.Simulations) (*models.Simulations, error) {
+	if err := r.db.Create(simulation).Error; err != nil {
+		return nil, err
+	}
+
+	return simulation, nil
 }
