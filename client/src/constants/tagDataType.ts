1b35dc5abbe04b6e967583d3187553c7b23960ec
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-07-16 15:28:32 +0800

    📝constants: tag data types

---
+++ client/src/constants/tagDataType.ts
@@ -0,0 +1,6 @@
+export const tagDataTypes = [
+	{ value: "BOOL", label: "Boolean" },
+	{ value: "INT", label: "Integer" },
+	{ value: "FLOAT", label: "Decimal" },
+	{ value: "STRING", label: "Text" },
+] as const;
