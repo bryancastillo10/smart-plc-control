44cbe47ab39b91f6dbffa0dcf4db1c01683856f0
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-07-17 12:44:40 +0800

    🔗route: connect stop simulations handler at routing

--- server/bootstrap/routes.go
+++ server/bootstrap/routes.go
@@ -194,7 +194,7 @@ func registerSimulations(r *gin.RouterGroup, DB *gorm.DB) {
 		simulationGrp.DELETE("/:simulationId", middleware.RequireRoles(models.Admin), simulation.DeleteSimulation)
 		simulationGrp.POST("/:simulationId/start", middleware.RequireRoles(models.Admin, models.Operator), simulation.StartSimulation)
 		simulationGrp.POST("/:simulationId/pause", middleware.RequireRoles(models.Admin, models.Operator), simulation.PauseSimulation)
-		simulationGrp.POST("/:simulationId/stop")
+		simulationGrp.POST("/:simulationId/stop", middleware.RequireRoles(models.Admin, models.Operator), simulation.StopSimulation)
 		simulationGrp.POST("/:simulationId/scenarios")
 		simulationGrp.POST("/:simulationId/scenarios/:scenarioId/trigger")
 	}
