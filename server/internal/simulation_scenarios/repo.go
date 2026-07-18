4bb9f7376476f1a0675bd574fac32b63dacfc139
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2026-07-18 20:16:22 +0800

    🌐api: create simulation scenario repo layer

--- server/internal/simulation_scenarios/repo.go
+++ server/internal/simulation_scenarios/repo.go
@@ -1,6 +1,11 @@
 package simulation_scenarios
 
-import "gorm.io/gorm"
+import (
+	"smart-plc-control-server/internal/models"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 type Repository struct {
 	db *gorm.DB
@@ -8,4 +13,24 @@ type Repository struct {
 
 func NewRepository(db *gorm.DB) *Repository {
 	return &Repository{db: db}
+}
+
+func (r *Repository) FindSimulationByID(simulationID uuid.UUID) (*models.Simulations, error) {
+	var simulation models.Simulations
+	if err := r.db.Where("id = ?", simulationID).First(&simulation).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return &simulation, nil
+}
+
+func (r *Repository) CreateSimulationScenario(scenario *models.SimulationScenarios) (*models.SimulationScenarios, error) {
+	if err := r.db.Create(scenario).Error; err != nil {
+		return nil, err
+	}
+
+	return scenario, nil
 }
