910e029ef93fede23073896fe0cc19c7e07307da
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2026-07-18 20:17:35 +0800

    🔗route: connect create simulation scenario handler at routing

--- server/bootstrap/routes.go
+++ server/bootstrap/routes.go
@@ -13,6 +13,7 @@ import (
 	"smart-plc-control-server/internal/models"
 	"smart-plc-control-server/internal/plants"
 	"smart-plc-control-server/internal/process_units"
+	"smart-plc-control-server/internal/simulation_scenarios"
 	"smart-plc-control-server/internal/simulations"
 	"smart-plc-control-server/internal/tag_readings"
 	"smart-plc-control-server/internal/tags"
@@ -184,6 +185,7 @@ func registerAlerts(r *gin.RouterGroup, DB *gorm.DB) {
 
 func registerSimulations(r *gin.RouterGroup, DB *gorm.DB) {
 	simulation := simulations.NewHandler(DB)
+	simulationScenario := simulation_scenarios.NewHandler(DB)
 
 	simulationGrp := r.Group("/simulations", middleware.JWTAuthMiddleware())
 	{
@@ -197,7 +199,7 @@ func registerSimulations(r *gin.RouterGroup, DB *gorm.DB) {
 		simulationGrp.POST("/:simulationId/stop", middleware.RequireRoles(models.Admin, models.Operator), simulation.StopSimulation)
 
 		// Simulation Scenarios
-		simulationGrp.POST("/:simulationId/scenarios")
+		simulationGrp.POST("/:simulationId/scenarios", middleware.RequireRoles(models.Admin), simulationScenario.CreateSimulationScenario)
 		simulationGrp.POST("/:simulationId/scenarios/:scenarioId/trigger")
 	}
 }
