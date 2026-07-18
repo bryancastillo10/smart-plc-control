029db749fbdecffc0f0a4a44f0c81f8f19d79006
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2026-07-18 20:17:11 +0800

    🌐api: create simulation scenario handler layer

--- server/internal/simulation_scenarios/handler.go
+++ server/internal/simulation_scenarios/handler.go
@@ -1,6 +1,11 @@
 package simulation_scenarios
 
-import "gorm.io/gorm"
+import (
+	"smart-plc-control-server/pkg/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
 
 type Handler struct {
 	service *Service
@@ -10,4 +15,22 @@ func NewHandler(db *gorm.DB) *Handler {
 	repo := NewRepository(db)
 	service := NewService(repo)
 	return &Handler{service: service}
+}
+
+func (h *Handler) CreateSimulationScenario(c *gin.Context) {
+	simulationID := c.Param("simulationId")
+
+	req, err := http.BindJSON[CreateSimulationScenarioRequest](c)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	scenario, err := h.service.CreateSimulationScenario(simulationID, *req)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	c.JSON(201, scenario)
 }
